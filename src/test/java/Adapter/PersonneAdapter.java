370390059867a8d4275d7a50d100407705bb00b4
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-29 10:53:44 +0200

    PersonneAdapter added was born

--- src/test/java/Adapter/PersonneAdapter.java
+++ src/test/java/Adapter/PersonneAdapter.java
@@ -4,32 +4,29 @@ import people.Personne;
 import persons.IPerson;
 import java.util.GregorianCalendar;
 
-public class PersonneAdapter implements IPerson {
-
-    private Personne personne;
+public class PersonneAdapter extends Personne implements IPerson {
 
     public PersonneAdapter (String var1, String var2, int var3, int var4, int var5) {
-        this.personne = new Personne(var1, var2, var3, var4, var5);
+        super(var1, var2, var3, var4, var5);
     }
 
     @Override
     public String getName() {
-        return personne.getName();
+        return super.getName();
     }
 
     @Override
     public String getFirstName() {
-        return personne.getFirstName();
+        return super.getFirstName();
     }
 
     @Override
     public boolean wasBorn(GregorianCalendar gregorianCalendar) {
-        //TODO Personne ne possède pas cette méthode
-        return true;
+        return gregorianCalendar.before(super.dateNaissance);
     }
 
     @Override
     public int getAge(GregorianCalendar gregorianCalendar) {
-        return personne.getAge(gregorianCalendar);
+        return super.getAge(gregorianCalendar);
     }
 }
