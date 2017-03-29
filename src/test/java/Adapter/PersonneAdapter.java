f83e404f876e65cb27277e0b12bf4860e17a3428
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-29 10:41:06 +0200

    constructor

--- src/test/java/Adapter/PersonneAdapter.java
+++ src/test/java/Adapter/PersonneAdapter.java
@@ -2,14 +2,15 @@ package Adapter;
 
 import people.Personne;
 import persons.IPerson;
-
 import java.util.GregorianCalendar;
-
 
 public class PersonneAdapter implements IPerson {
 
     private Personne personne;
 
+    public PersonneAdapter (String var1, String var2, int var3, int var4, int var5) {
+        this.personne = new Personne(var1, var2, var3, var4, var5);
+    }
 
     @Override
     public String getName() {
