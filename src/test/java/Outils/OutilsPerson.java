2468c2ab1cd87a5bd8efcac0226e7f60991d3049
GregoirePiat <gregoire.piat@outlook.fr>
2017-03-29 10:59:29 +0200

    Fixed issue on OutilsPerson

--- src/test/java/Outils/OutilsPerson.java
+++ src/test/java/Outils/OutilsPerson.java
@@ -12,6 +12,9 @@ public class OutilsPerson {
 
         List<IPerson> personsOnRange = new ArrayList<>();
 
+        if (persons.size() == 0)
+            return new ArrayList<>();
+
         for (IPerson person : persons){
             if (person.getAge(date) >= minAge && person.getAge(date) <= maxAge)
                 personsOnRange.add(person);
@@ -24,6 +27,9 @@ public class OutilsPerson {
     public int maxAge(List<IPerson> persons, GregorianCalendar date){
 
         int maxAge = -1;
+
+        if (persons.size() == 0)
+            return -1;
 
         for (IPerson person : persons){
             if (person.getAge(date) > maxAge)
