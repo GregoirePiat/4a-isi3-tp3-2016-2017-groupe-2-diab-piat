c18922e24b1bccb299605e6fd8a2420428dc4598
GregoirePiat <gregoire.piat@outlook.fr>
2017-03-29 08:48:22 +0200

    Refactored tests

--- RAPPORT.md
+++ RAPPORT.md
@@ -1,6 +1,34 @@
-**Nom/Prénom Etudiant 1 :**
+**Nom/Prénom Etudiant 1 :** DIAB Nicolas
 
-**Nom/Prénom Etudiant 2 :**
+**Nom/Prénom Etudiant 2 :** PIAT Grégoire
 
 # Rapport TP3
+
+## Question 1
+Nous allons tester si les méthodes getAge et wasBorn dans les cas suivants :
+- Au début d'année de sa naissance
+- A la fin d'année de sa naissance
+- 10 ans avant sa naissance
+- 10 ans après sa naissance
+
+@TODO ajouter les classes d'équivalence
+
+
+## Question 2
+
+## Question 3
+
+## Question 4
+
+## Question 5
+
+## Question 6
+
+## Question 7
+
+## Question 8
+
+## Question 9
+
+## Question 10
 
--- src/test/java/PersonneTest.java
+++ src/test/java/PersonneTest.java
@@ -8,7 +8,7 @@ import java.util.GregorianCalendar;
 public class PersonneTest {
 
 	@Test
-	public void test() {
+	public void testGetAge() {
 		GregorianCalendar date2010 = new GregorianCalendar(2010, 01, 01);
 		GregorianCalendar date2000Janv = new GregorianCalendar(2000, 01, 01);
 		GregorianCalendar date2000Dec = new GregorianCalendar(2000, 01, 12);
@@ -20,15 +20,12 @@ public class PersonneTest {
 
 		// 10 ans après sa naissance
 		assertThat(person2000.getAge(date2010)).isEqualTo(10);
-		assertThat(person2000.wasBorn(date2010)).isTrue();
 
 		// A sa naissance - début année
 		assertThat(person2000.getAge(date2000Janv)).isEqualTo(0);
-		assertThat(person2000.wasBorn(date2000Janv)).isTrue();
 
 		// A sa naissance - fin année
 		assertThat(person2000.getAge(date2000Dec)).isEqualTo(0);
-		assertThat(person2000.wasBorn(date2000Dec)).isTrue();
 
 		// 10 ans avant sa naissance
 		try {
@@ -37,6 +34,29 @@ public class PersonneTest {
 		catch (IllegalArgumentException e) {
 			assertThat(e.getMessage()).isNotEmpty();
 		}
+	}
+
+	@Test
+	public void testWasBorn() {
+		GregorianCalendar date2010 = new GregorianCalendar(2010, 01, 01);
+		GregorianCalendar date2000Janv = new GregorianCalendar(2000, 01, 01);
+		GregorianCalendar date2000Dec = new GregorianCalendar(2000, 01, 12);
+		GregorianCalendar date1990 = new GregorianCalendar(1990, 01, 01);
+
+
+		//Premier constructeur
+		Person person2000 = new Person("lastname", "firstname", 2000, 01, 01);
+
+		// 10 ans après sa naissance
+		assertThat(person2000.wasBorn(date2010)).isTrue();
+
+		// A sa naissance - début année
+		assertThat(person2000.wasBorn(date2000Janv)).isTrue();
+
+		// A sa naissance - fin année
+		assertThat(person2000.wasBorn(date2000Dec)).isTrue();
+
+		// 10 ans avant sa naissance
 		assertThat(person2000.wasBorn(date1990)).isFalse();
 	}
 }
