8087481c95080da19d9ff916a66bace5e5f1e05b
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-29 08:36:17 +0200

    Question 1 - ajout try catch + enlevé le 2ème constructeur

--- src/test/java/PersonneTest.java
+++ src/test/java/PersonneTest.java
@@ -31,27 +31,12 @@ public class PersonneTest {
 		assertThat(person2000.wasBorn(date2000Dec)).isTrue();
 
 		// 10 ans avant sa naissance
-		// assertThat(person2000.getAge(date1990)).isEqualTo(0); - la méthode lance une exception
-		assertThat(person2000.wasBorn(date1990)).isFalse();
-
-
-		// deuxième contructeur
-		person2000 = new Person("lastname", "firstname", date2000Janv);
-
-		// 10 ans après sa naissance
-		assertThat(person2000.getAge(date2010)).isEqualTo(10);
-		assertThat(person2000.wasBorn(date2010)).isTrue();
-
-		// A sa naissance - début année
-		assertThat(person2000.getAge(date2000Janv)).isEqualTo(0);
-		assertThat(person2000.wasBorn(date2000Janv)).isTrue();
-
-		// A sa naissance - fin année
-		assertThat(person2000.getAge(date2000Dec)).isEqualTo(0);
-		assertThat(person2000.wasBorn(date2000Dec)).isTrue();
-
-		// 10 ans avant sa naissance
-		// assertThat(person2000.getAge(date1990)).isEqualTo(0); - la méthode lance une exception
+		try {
+			assertThat(person2000.getAge(date1990)).isEqualTo(0);
+		}
+		catch (IllegalArgumentException e) {
+			assertThat(e.getMessage()).isNotEmpty();
+		}
 		assertThat(person2000.wasBorn(date1990)).isFalse();
 	}
 }
