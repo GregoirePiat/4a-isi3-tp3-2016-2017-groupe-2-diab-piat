df75d5f25e8534d51d02f13327ef3ce8355a57b2
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-29 09:14:46 +0200

    yo

---
+++ src/test/java/PeopleTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class PeopleTest
+{
+    private PersonneTest personneTest;
+
+    @Test
+    public void testGetAge() {
+        personneTest = new PersonneTest();
+      //  personneTest.setPerson2000();
+        personneTest.testGetAge();
+    }
+
+    @Test
+    public void testWasBorn() {
+
+    }
+}
--- src/test/java/PersonneTest.java
+++ src/test/java/PersonneTest.java
@@ -7,6 +7,8 @@ import java.util.GregorianCalendar;
 
 public class PersonneTest {
 
+	private Person person2000;
+
 	@Test
 	public void testGetAge() {
 		GregorianCalendar date2010 = new GregorianCalendar(2010, 01, 01);
@@ -15,21 +17,22 @@ public class PersonneTest {
 		GregorianCalendar date1990 = new GregorianCalendar(1990, 01, 01);
 
 
-		//Premier constructeur
-		Person person2000 = new Person("lastname", "firstname", 2000, 01, 01);
+		//Premier constructeur - permet de run directement la classe sans erreur
+		if (this.getPerson2000() == null)
+		 	this.setPerson2000(new Person("lastname", "firstname", 2000, 01, 01));
 
 		// 10 ans après sa naissance
-		assertThat(person2000.getAge(date2010)).isEqualTo(10);
+		assertThat(getPerson2000().getAge(date2010)).isEqualTo(10);
 
 		// A sa naissance - début année
-		assertThat(person2000.getAge(date2000Janv)).isEqualTo(0);
+		assertThat(getPerson2000().getAge(date2000Janv)).isEqualTo(0);
 
 		// A sa naissance - fin année
-		assertThat(person2000.getAge(date2000Dec)).isEqualTo(0);
+		assertThat(getPerson2000().getAge(date2000Dec)).isEqualTo(0);
 
 		// 10 ans avant sa naissance
 		try {
-			assertThat(person2000.getAge(date1990)).isEqualTo(0);
+			assertThat(getPerson2000().getAge(date1990)).isEqualTo(0);
 		}
 		catch (IllegalArgumentException e) {
 			assertThat(e.getMessage()).isNotEmpty();
@@ -44,8 +47,9 @@ public class PersonneTest {
 		GregorianCalendar date1990 = new GregorianCalendar(1990, 01, 01);
 
 
-		//Premier constructeur
-		Person person2000 = new Person("lastname", "firstname", 2000, 01, 01);
+		//Premier constructeur - permet de run directement la classe sans erreur
+		if (this.getPerson2000() == null)
+			this.setPerson2000(new Person("lastname", "firstname", 2000, 01, 01));
 
 		// 10 ans après sa naissance
 		assertThat(person2000.wasBorn(date2010)).isTrue();
@@ -58,5 +62,13 @@ public class PersonneTest {
 
 		// 10 ans avant sa naissance
 		assertThat(person2000.wasBorn(date1990)).isFalse();
+	}
+
+	public Person getPerson2000() {
+		return person2000;
+	}
+
+	public void setPerson2000(Person person2000) {
+		this.person2000 = person2000;
 	}
 }
